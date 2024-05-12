6d5e88cdb36fc13c2e67fa9fee4db3f3aa3b471d
Alexandr Kolupaev <a.kolupaev@entangle.fi>
2024-05-12 10:32:08 +0300

    Implement config verification

--- config/config.go
+++ config/config.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"fmt"
+	"net/url"
+	"errors"
+	"regexp"
+
 	"github.com/kelseyhightower/envconfig"
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -12,8 +17,28 @@ type AppConfig struct {
 	PrivateKey string `envconfig:"PRIVATE_KEY" required:"true"`
 }
 
-func (config AppConfig) Verify() {
-	// @@@ TODO
+// This function verifies the symbolic correctness of the configuration. 
+// This however does not guarantee the semantic correctness of the configuration.
+func (config AppConfig) Verify() error {
+	// Check if URLs are valid
+	if _, err := url.ParseRequestURI(config.FinalizeSnapshotUrl); err != nil {
+		return fmt.Errorf("invalid FinalizeSnapshotUrl: %w", err)
+	}
+
+	if _, err := url.ParseRequestURI(config.TargetChainUrl); err != nil {
+		return fmt.Errorf("invalid TargetChainUrl: %w", err)
+	}
+
+	// Don't need to check common.Address
+	// if common.IsHexAddress(config.PullOracleAddress.Hex()) == false { }
+
+	// Check if PrivateKey is in the correct format (0x followed by 64 hex characters)
+	re := regexp.MustCompile(`^0x[a-fA-F0-9]{64}$`)
+	if !re.MatchString(config.PrivateKey) {
+		return errors.New("invalid PrivateKey: must be 0x[0-9a-zA-Z]{64}")
+	}
+
+	return nil
 }
 
 // LoadConfig reads environment variables and initializes an AppConfig struct.
--- config/config_test.go
+++ config/config_test.go
@@ -4,18 +4,24 @@ import (
 	"os"
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/brianvoe/gofakeit/v7"
 )
 
 func TestLoadConfigWithCorrectEnv(t *testing.T) {
 	// Setup environment variables
-	finalizeSnapshotUrl := "http://localhost:3000"
+	// finalizeSnapshotUrl := "http://localhost:3000"
+	finalizeSnapshotUrl := gofakeit.URL()
+	targetChainUrl := gofakeit.URL()
 	assert.Nil(t, os.Setenv("FINALIZE_SNAPSHOT_URL", finalizeSnapshotUrl))
-	assert.Nil(t, os.Setenv("TARGET_CHAIN_URL", finalizeSnapshotUrl))
+	assert.Nil(t, os.Setenv("TARGET_CHAIN_URL", targetChainUrl))
 	assert.Nil(t, os.Setenv("PULL_ORACLE_ADDRESS", "0x5ca636af0aB140A75515Bd708E3e382aa7A70aEb"))
 	assert.Nil(t, os.Setenv("PRIVATE_KEY", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")) // 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
 	// Load config
 	config, err := LoadConfigFromEnv()
+	assert.Nil(t, err)
+
+	err = config.Verify()
 	assert.Nil(t, err)
 
 	// Assert config values
